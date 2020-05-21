a71492bc86648dc05d1a79a980dd036aa84caaac
stuthemp <semenshulga1@gmail.com>
2020-05-21 13:32:23 +0000

    swap and revert done

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
 #include "revert_string.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	char  *revert_string;
+    revert_string = (char*)malloc(sizeof(char) * (strlen(str) + 1));
+    for(int i =0; i < strlen(str); i++){
+        revert_string[i] = str[strlen(str)-i-1];
+    }
+    strcpy(str,revert_string);
+    free(revert_string);
 }
 
